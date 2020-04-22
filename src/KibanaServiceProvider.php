7225ed10b645cfa7cdf30a46f27b12e6cd325ad5
Bilal Baraz <bilalbaraz@windowslive.com>
2020-04-23 02:09:53 +0300

    Increase to 100 unit test coverage

--- src/KibanaServiceProvider.php
+++ src/KibanaServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Bilalbaraz\LaravelKibana;
 
+use Bilalbaraz\LaravelKibana\Client\Kibana;
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -15,6 +17,10 @@ class KibanaServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(Kibana::class, function ($app) {
+            return new Kibana(new Client(), $app['config']['kibana']);
+        });
+
         $this->mergeConfigFrom(__DIR__ . '/../config/kibana.php', 'config');
     }
 
@@ -24,5 +30,13 @@ class KibanaServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([__DIR__ . '/../config/kibana.php' => config_path('kibana.php')], 'config');
+    }
+
+    /**
+     * @return array
+     */
+    public function provides(): array
+    {
+        return [Kibana::class];
     }
 }
